8b5b3897711309a18865695850fede29d4ea8e75
rinetrous <rinellie1228@icloud.com>
2021-05-04 10:12:10 +0900

    first commit

---
+++ TabBarControllerSample/TabBar/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  TabBarControllerSample
+//
+//  Created by 野田凜太郎 on 2021/05/04.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController{
+    
+}
